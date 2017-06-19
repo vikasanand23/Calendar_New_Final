3a81bd76979bc0c0c8a12e3e6ef68d3b99482bd3
Vikas Anand <vikasanand23@gmail.com>
2017-06-19 11:52:27 +0530

    chk
    chk

--- Calendar_New_Final/Calendar_One/com/directive/calendarDirective.js
+++ Calendar_New_Final/Calendar_One/com/directive/calendarDirective.js
@@ -4,7 +4,11 @@
             restrict: 'EA',
             //template: '<h3>Hello From Directive</h3>',
             //templateUrl: '../../com/view/directives/calendarResult.html',
-            replace: true
+            replace: true,
+			controller: 'calendarController'
+			link: function(scope, iElement, iAttributes, ctrl){
+			  
+			}
         }
     }
     angular.module("app").directive("customCalendar", customCalendarFunc)
