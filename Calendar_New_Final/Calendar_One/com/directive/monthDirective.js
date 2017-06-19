1ab7b2174bf1b47e87dfa9ef3052dd93ab38b0b0
Vikas Anand <vikasanand23@gmail.com>
2017-06-19 11:53:16 +0530

    CHK
    chk

--- Calendar_New_Final/Calendar_One/com/directive/monthDirective.js
+++ Calendar_New_Final/Calendar_One/com/directive/monthDirective.js
@@ -1,6 +1,12 @@
 angular.module("app").directive('myMonth', function() {
     return {
         restrict: 'EA',
-        templateUrl: 'com/view/month.html'
+        templateUrl: 'com/view/month.html',
+		transclude: true,
+		controller: 'calendarController',
+		link: function(scope, iElement, iAttributes, ctrl){
+			  
+			}
+    	//scope: {},
     };
 });
